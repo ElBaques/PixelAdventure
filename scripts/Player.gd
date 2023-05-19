0e6644303d8c53317433164415f4046339f9b266
Thales Hott Costa <thaleshott.hc@gmail.com>
2023-05-19 15:38:22 -0300

    segundo commit

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var vel = Vector2.ZERO
-var move_speed = 480
+var move_speed = 180
 var gravity = 1200
 var jump_force = -720
 var is_grounded
@@ -15,8 +15,10 @@ func _physics_process(delta: float) -> void:
 	vel = move_and_slide(vel)
 	
 	is_grounded = _check_is_ground()
+	_set_animation()
 
 func _get_input():
+	vel.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	vel.x = move_speed * move_direction
 	
@@ -35,8 +37,12 @@ func _check_is_ground():
 	return false		
 
 func _set_animation():
-	var anim = "idle"
+	var anim = "Idle"
 	
 	if !is_grounded:
-		anim = "jump"
+		anim = "Jump"
+	elif vel.x !=0:
+			anim = "run"
 	
+	if $anim.assigned_animation	!= anim:
+		$anim.play(anim)
